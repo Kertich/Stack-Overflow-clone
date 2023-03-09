876611f256b74f150c70ff9d7d6e1f9d47018ba2
Kertich <kipsangkertich@gmail.com>
2023-03-09 17:10:42 +0300

    add sign up

---
+++ Frontend/src/app/components/signup/signup.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+  signupForm!: FormGroup;
+  public googleImg = "https://img.icons8.com/fluency/1x/google-logo.png"
+  public gitImg = "https://img.icons8.com/fluency-systems-regular/1x/github.png"
+  public fbImg = "https://img.icons8.com/color/256/facebook.png"
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.signupForm.invalid) {
+      return;
+    }
+
+    // Perform signin logic here
+  }
+}
