f8a87c736c52ea9e5f9cb0da27f3a1ee478d0f87
Kertich <kipsangkertich@gmail.com>
2023-03-12 12:54:47 +0300

    use guards to protect ask question page

---
+++ Frontend/src/app/components/ask-question/ask-question.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { Question } from 'src/app/models/question.model';
+
+@Component({
+  selector: 'app-ask-question',
+  templateUrl: './ask-question.component.html',
+  styleUrls: ['./ask-question.component.css']
+})
+export class AskQuestionComponent {
+  title: string = '';
+  details: string = '';
+  tried: string = '';
+  expecting: string = '';
+  tags: string = '';
+
+  onSubmit() {
+    const question: Question = {
+      id: 1,
+      title: this.title,
+      body: this.details,
+      tags: this.tags.split(','),
+      created_at: new Date()
+    };
+    
+    // TODO: send question to backend API to save in database
+
+    // Reset form fields
+    this.title = '';
+    this.details = '';
+    this.tried = '';
+    this.expecting = '';
+    this.tags = '';
+  }
+}
+
+
+
